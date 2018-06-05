3ae11eb6fb6b901fac7a70a2a18068b2eed7084b
ingarcia1 <ingarcia1>
2018-06-04 23:44:38 -0600

    stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+//Code implementation in android studio
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
